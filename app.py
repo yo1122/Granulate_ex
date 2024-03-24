9bd6c01794ad61037b0e6e43c29248cfa2c85a67
Yoav Levy <yo1122@gmail.com>
2024-03-24 17:46:47 +0200

    milestone4, more partial than before, notice TODOs

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import ssl
 
 from flask import Flask, jsonify, request
 
-from Messages import Message, init_db
-from constants import POST, GET, SUCCESS, ERROR, MESSAGE, SERVER_HOST, SERVER_PORT, FAIL_VALUE
+from models.messaging import Message, ChatRoom
+from models.users import User
+from models.utils import init_db, db
+from constants import POST, GET, SUCCESS, ERROR, MESSAGE, SERVER_HOST, SERVER_PORT, FAIL_VALUE, GET_CHATROOMS, CHATROOM
 
 app = Flask(__name__)
 init_db(app)
@@ -20,7 +22,8 @@ def run_server():
     app.run(ssl_context=context, debug=True, host=host, port=port)
 
 
-def verify_incoming_message(request):
+@app.route('/send_message', methods=[POST])
+def send_message():
     # use auth data for milestone 6
     auth = request.authorization
     if not auth:
@@ -29,28 +32,46 @@ def verify_incoming_message(request):
     if not username:
         return jsonify({ERROR: 'Username is required.'}), 401
     data = request.get_json()
-    if MESSAGE not in data:
-        return jsonify({ERROR: 'Message is required.'}), 400
-    return data, FAIL_VALUE
+    username = username.decode("utf-8") if isinstance(username, bytes) else username
+    if MESSAGE not in data or CHATROOM not in data:
+        return jsonify({ERROR: 'Message and chatroom are required.'}), 400
 
-
-@app.route('/send_message', methods=[POST])
-def send_message():
-    data, code = verify_incoming_message(request)
-    if code != FAIL_VALUE:
-        return data, code
-
-    username = request.authorization.username
-    message_text = data[MESSAGE]
-    Message.message_to_db(username, message_text)
+    message_text = data.get(MESSAGE)
+    chatroom = data.get("chatroom")
+    Message.message_to_db(username, message_text, chatroom)
 
     return jsonify({SUCCESS: True}), 201
 
 
 @app.route('/get_messages', methods=[GET])
 def get_messages():
-    messages = Message.get_all_unseen()
+    # use auth data for milestone 6
+    auth = request.authorization
+    if not auth:
+        return jsonify({ERROR: 'Please add authorization.'}), 401
+    username = auth.username
+    if not username:
+        return jsonify({ERROR: 'Username is required.'}), 401
+    username = username.decode("utf-8") if isinstance(username, bytes) else username
+    data = request.get_json()
+    if CHATROOM not in data:
+        return jsonify({ERROR: 'Chatroom is required.'}), 400
+
+    chatroom_name = data.get("chatroom")
+    user = User.get_or_create(username)
+
+    if db.session.query(ChatRoom.query.filter_by(name=chatroom_name).exists()).scalar():
+        messages = Message.get_all_unseen(username, chatroom_name)
+    else:
+        ChatRoom.chatroom_to_db(chatroom_name, user)
+        messages = list()
     return jsonify(messages)
+
+
+@app.route(f'/{GET_CHATROOMS}', methods=[GET])
+def get_chatrooms():
+    result = ChatRoom.get_all_chatrooms()
+    return jsonify(result)
 
 
 if __name__ == '__main__':
--- client.py
+++ client.py
@@ -3,19 +3,74 @@ import os
 import requests
 import threading
 import time
-
-from constants import POLL_TIME, SERVER_HOST, SERVER_PORT
+from constants import POLL_TIME, SERVER_HOST, SERVER_PORT, GET_CHATROOMS
 
 SERVER_URL = f"https://{os.environ.get(SERVER_HOST, '0.0.0.0')}:{os.environ.get(SERVER_PORT, 5000)}"
 
+# TODO: handle ssl certificate smarter
+# TODO: test db dockerfile
+# TODO: go over all env vars and constants, especially if using const in several important places
+# TODO: verify incoming message, especially format (unicode?)
+# TODO: test per (main funcs/cases)
+
+
+def get_messages(stop_event, username, chatroom):
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    #  WARNING: Verify false for debug only
+    print("Messages:")
+    while not stop_event.is_set():
+        data = {"chatroom": chatroom}
+        response = requests.get(f"{SERVER_URL}/get_messages", json=data, auth=(username, ""),verify=False)
+        if response.status_code == 200:
+            messages = response.json()
+            for message in messages:
+                print(f"{message['username']}: {message['message']}")
+        else:
+            print("Failed to fetch messages.")
+        time.sleep(POLL_TIME)
+
+
+def send_message(stop_event, username, chatroom):
+    #  WARNING: Verify false for debug only
+
+    while not stop_event.is_set():
+        message = input("Enter your message: ")
+        data = {"message": message, "chatroom": chatroom}
+        response = requests.post(f"{SERVER_URL}/send_message", json=data, auth=(username, ""), verify=False)
+        if response.status_code == 201:
+            print("Message sent successfully.")
+        else:
+            print("Failed to send message.")
+
+
+def request_chatrooms():
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    response = requests.get(f"{SERVER_URL}/{GET_CHATROOMS}", verify=False)
+    if response.status_code == 200:
+        chatrooms = response.json()
+        print(f"Available chatrooms: {', '.join(chatrooms)}")
+    else:
+        print("Failed to fetch chat rooms.")
+
+
+def start_all_threads(username, chatroom):
+    stop_event = threading.Event()
+    get_messages_thread = threading.Thread(target=get_messages, args=(stop_event, username, chatroom))
+    send_message_thread = threading.Thread(target=send_message, args=(stop_event, username, chatroom))
+    get_messages_thread.start()
+    send_message_thread.start()
+    return get_messages_thread, send_message_thread, stop_event
 
 def run_client():
     username = input("Enter your username: ")
-    stop_event = threading.Event()
-    get_messages_thread = threading.Thread(target=get_messages, args=(stop_event,))
-    send_message_thread = threading.Thread(target=send_message, args=(username, stop_event))
-    get_messages_thread.start()
-    send_message_thread.start()
+    request_chatrooms()
+    chatroom = input("Type name of existing or new chatroom to connect to: ")
+
+    get_messages_thread, send_message_thread, stop_event = start_all_threads(username, chatroom)
 
     try:
         get_messages_thread.join()
@@ -27,36 +82,6 @@ def run_client():
         stop_event.set()
         get_messages_thread.join()
         send_message_thread.join()
-
-
-def get_messages(stop_event):
-    import urllib3
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-    #  WARNING: Verify false for debug only
-    print("Messages:")
-    while not stop_event.is_set():
-        response = requests.get(f"{SERVER_URL}/get_messages", verify=False)
-        if response.status_code == 200:
-            messages = response.json()
-            for message in messages:
-                print(f"{message['username']}: {message['message']}")
-        else:
-            print("Failed to fetch messages.")
-        time.sleep(POLL_TIME)
-
-
-def send_message(username, stop_event):
-    #  WARNING: Verify false for debug only
-
-    while not stop_event.is_set():
-        message = input("Enter your message: ")
-        data = {"message": message}
-        response = requests.post(f"{SERVER_URL}/send_message", json=data, auth=(username, ""), verify=False)
-        if response.status_code == 201:
-            print("Message sent successfully.")
-        else:
-            print("Failed to send message.")
 
 
 if __name__ == "__main__":
